47cbce2eaa8cd324a7ad66e42a5e0bb68b72a756
Jocob Hong <hungkaojay@gmail.com>
2024-08-03 00:38:39 +0800

    Finish CustSYSForm

--- PharmacyShop/CustSYSForm.Designer.cs
+++ PharmacyShop/CustSYSForm.Designer.cs
@@ -35,20 +35,23 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnNoright = new System.Windows.Forms.Button();
             this.btnDrop = new System.Windows.Forms.Button();
-            this.btnCustInsert = new System.Windows.Forms.Button();
+            this.btnCustUpdate = new System.Windows.Forms.Button();
             this.dataCust = new Guna.UI2.WinForms.Guna2DataGridView();
             this.txtphone = new System.Windows.Forms.TextBox();
             this.lblphone = new System.Windows.Forms.Label();
             this.txtAddress = new System.Windows.Forms.TextBox();
             this.txtEmail = new System.Windows.Forms.TextBox();
-            this.txtPass = new System.Windows.Forms.TextBox();
             this.txtUser = new System.Windows.Forms.TextBox();
             this.lbladdress = new System.Windows.Forms.Label();
             this.lblemail = new System.Windows.Forms.Label();
-            this.lblpass = new System.Windows.Forms.Label();
             this.lbluser = new System.Windows.Forms.Label();
             this.txtid = new System.Windows.Forms.TextBox();
             this.lblid = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cbxSearch = new System.Windows.Forms.ComboBox();
+            this.txtkeyword = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnShowAll = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataCust)).BeginInit();
             this.SuspendLayout();
@@ -56,13 +59,15 @@
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.Gray;
-            this.groupBox1.Controls.Add(this.btnNoright);
-            this.groupBox1.Controls.Add(this.btnDrop);
-            this.groupBox1.Controls.Add(this.btnCustInsert);
+            this.groupBox1.Controls.Add(this.btnShowAll);
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.cbxSearch);
+            this.groupBox1.Controls.Add(this.txtkeyword);
+            this.groupBox1.Controls.Add(this.label9);
             this.groupBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.groupBox1.Location = new System.Drawing.Point(58, 324);
+            this.groupBox1.Location = new System.Drawing.Point(30, 303);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(260, 158);
+            this.groupBox1.Size = new System.Drawing.Size(303, 179);
             this.groupBox1.TabIndex = 49;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "會員管理";
@@ -70,7 +75,7 @@
             // btnNoright
             // 
             this.btnNoright.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnNoright.Location = new System.Drawing.Point(31, 102);
+            this.btnNoright.Location = new System.Drawing.Point(252, 263);
             this.btnNoright.Name = "btnNoright";
             this.btnNoright.Size = new System.Drawing.Size(81, 34);
             this.btnNoright.TabIndex = 49;
@@ -81,7 +86,7 @@
             // btnDrop
             // 
             this.btnDrop.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnDrop.Location = new System.Drawing.Point(142, 51);
+            this.btnDrop.Location = new System.Drawing.Point(141, 263);
             this.btnDrop.Name = "btnDrop";
             this.btnDrop.Size = new System.Drawing.Size(81, 34);
             this.btnDrop.TabIndex = 48;
@@ -89,16 +94,16 @@
             this.btnDrop.UseVisualStyleBackColor = true;
             this.btnDrop.Click += new System.EventHandler(this.btnDrop_Click);
             // 
-            // btnCustInsert
+            // btnCustUpdate
             // 
-            this.btnCustInsert.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnCustInsert.Location = new System.Drawing.Point(31, 51);
-            this.btnCustInsert.Name = "btnCustInsert";
-            this.btnCustInsert.Size = new System.Drawing.Size(81, 34);
-            this.btnCustInsert.TabIndex = 47;
-            this.btnCustInsert.Text = "新增";
-            this.btnCustInsert.UseVisualStyleBackColor = true;
-            this.btnCustInsert.Click += new System.EventHandler(this.btnCustInsert_Click);
+            this.btnCustUpdate.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnCustUpdate.Location = new System.Drawing.Point(30, 263);
+            this.btnCustUpdate.Name = "btnCustUpdate";
+            this.btnCustUpdate.Size = new System.Drawing.Size(81, 34);
+            this.btnCustUpdate.TabIndex = 47;
+            this.btnCustUpdate.Text = "修改";
+            this.btnCustUpdate.UseVisualStyleBackColor = true;
+            this.btnCustUpdate.Click += new System.EventHandler(this.btnCustUpdate_Click);
             // 
             // dataCust
             // 
@@ -109,7 +114,6 @@
             dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Black;
             this.dataCust.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            this.dataCust.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(64)))));
             dataGridViewCellStyle2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
@@ -173,9 +177,8 @@
             // 
             this.txtphone.BackColor = System.Drawing.Color.White;
             this.txtphone.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.txtphone.Location = new System.Drawing.Point(105, 170);
+            this.txtphone.Location = new System.Drawing.Point(104, 128);
             this.txtphone.Name = "txtphone";
-            this.txtphone.ReadOnly = true;
             this.txtphone.Size = new System.Drawing.Size(186, 29);
             this.txtphone.TabIndex = 63;
             // 
@@ -186,7 +189,7 @@
             this.lblphone.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblphone.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.lblphone.ForeColor = System.Drawing.Color.Black;
-            this.lblphone.Location = new System.Drawing.Point(27, 173);
+            this.lblphone.Location = new System.Drawing.Point(26, 131);
             this.lblphone.Name = "lblphone";
             this.lblphone.Size = new System.Drawing.Size(42, 21);
             this.lblphone.TabIndex = 62;
@@ -196,9 +199,8 @@
             // 
             this.txtAddress.BackColor = System.Drawing.Color.White;
             this.txtAddress.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.txtAddress.Location = new System.Drawing.Point(105, 241);
+            this.txtAddress.Location = new System.Drawing.Point(104, 196);
             this.txtAddress.Name = "txtAddress";
-            this.txtAddress.ReadOnly = true;
             this.txtAddress.Size = new System.Drawing.Size(272, 29);
             this.txtAddress.TabIndex = 61;
             // 
@@ -206,30 +208,17 @@
             // 
             this.txtEmail.BackColor = System.Drawing.Color.White;
             this.txtEmail.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.txtEmail.Location = new System.Drawing.Point(105, 205);
+            this.txtEmail.Location = new System.Drawing.Point(104, 162);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.ReadOnly = true;
             this.txtEmail.Size = new System.Drawing.Size(272, 29);
             this.txtEmail.TabIndex = 60;
-            // 
-            // txtPass
-            // 
-            this.txtPass.BackColor = System.Drawing.Color.White;
-            this.txtPass.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.txtPass.Location = new System.Drawing.Point(105, 135);
-            this.txtPass.Name = "txtPass";
-            this.txtPass.PasswordChar = '●';
-            this.txtPass.ReadOnly = true;
-            this.txtPass.Size = new System.Drawing.Size(186, 29);
-            this.txtPass.TabIndex = 59;
             // 
             // txtUser
             // 
             this.txtUser.BackColor = System.Drawing.Color.White;
             this.txtUser.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.txtUser.Location = new System.Drawing.Point(105, 100);
+            this.txtUser.Location = new System.Drawing.Point(104, 94);
             this.txtUser.Name = "txtUser";
-            this.txtUser.ReadOnly = true;
             this.txtUser.Size = new System.Drawing.Size(186, 29);
             this.txtUser.TabIndex = 58;
             // 
@@ -240,7 +229,7 @@
             this.lbladdress.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lbladdress.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.lbladdress.ForeColor = System.Drawing.Color.Black;
-            this.lbladdress.Location = new System.Drawing.Point(27, 244);
+            this.lbladdress.Location = new System.Drawing.Point(26, 199);
             this.lbladdress.Name = "lbladdress";
             this.lbladdress.Size = new System.Drawing.Size(42, 21);
             this.lbladdress.TabIndex = 56;
@@ -253,24 +242,11 @@
             this.lblemail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblemail.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.lblemail.ForeColor = System.Drawing.Color.Black;
-            this.lblemail.Location = new System.Drawing.Point(27, 208);
+            this.lblemail.Location = new System.Drawing.Point(26, 165);
             this.lblemail.Name = "lblemail";
             this.lblemail.Size = new System.Drawing.Size(51, 21);
             this.lblemail.TabIndex = 55;
             this.lblemail.Text = "Email";
-            // 
-            // lblpass
-            // 
-            this.lblpass.AutoSize = true;
-            this.lblpass.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.lblpass.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.lblpass.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.lblpass.ForeColor = System.Drawing.Color.Black;
-            this.lblpass.Location = new System.Drawing.Point(27, 138);
-            this.lblpass.Name = "lblpass";
-            this.lblpass.Size = new System.Drawing.Size(42, 21);
-            this.lblpass.TabIndex = 54;
-            this.lblpass.Text = "密碼";
             // 
             // lbluser
             // 
@@ -279,7 +255,7 @@
             this.lbluser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lbluser.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.lbluser.ForeColor = System.Drawing.Color.Black;
-            this.lbluser.Location = new System.Drawing.Point(27, 103);
+            this.lbluser.Location = new System.Drawing.Point(26, 97);
             this.lbluser.Name = "lbluser";
             this.lbluser.Size = new System.Drawing.Size(58, 21);
             this.lbluser.TabIndex = 53;
@@ -289,7 +265,7 @@
             // 
             this.txtid.BackColor = System.Drawing.Color.DarkGray;
             this.txtid.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.txtid.Location = new System.Drawing.Point(105, 65);
+            this.txtid.Location = new System.Drawing.Point(104, 60);
             this.txtid.Name = "txtid";
             this.txtid.ReadOnly = true;
             this.txtid.Size = new System.Drawing.Size(77, 29);
@@ -302,11 +278,66 @@
             this.lblid.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblid.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.lblid.ForeColor = System.Drawing.Color.Black;
-            this.lblid.Location = new System.Drawing.Point(27, 68);
+            this.lblid.Location = new System.Drawing.Point(26, 63);
             this.lblid.Name = "lblid";
             this.lblid.Size = new System.Drawing.Size(27, 21);
             this.lblid.TabIndex = 64;
             this.lblid.Text = "ID";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnSearch.Location = new System.Drawing.Point(197, 84);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(78, 34);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "搜尋";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // cbxSearch
+            // 
+            this.cbxSearch.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cbxSearch.FormattingEnabled = true;
+            this.cbxSearch.Location = new System.Drawing.Point(184, 48);
+            this.cbxSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.cbxSearch.Name = "cbxSearch";
+            this.cbxSearch.Size = new System.Drawing.Size(92, 31);
+            this.cbxSearch.TabIndex = 9;
+            // 
+            // txtkeyword
+            // 
+            this.txtkeyword.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtkeyword.Location = new System.Drawing.Point(27, 48);
+            this.txtkeyword.Margin = new System.Windows.Forms.Padding(2);
+            this.txtkeyword.Name = "txtkeyword";
+            this.txtkeyword.Size = new System.Drawing.Size(154, 32);
+            this.txtkeyword.TabIndex = 8;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label9.ForeColor = System.Drawing.Color.Blue;
+            this.label9.Location = new System.Drawing.Point(29, 25);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(122, 21);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "欄位關鍵字搜尋";
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnShowAll.Location = new System.Drawing.Point(134, 122);
+            this.btnShowAll.Margin = new System.Windows.Forms.Padding(2);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(141, 34);
+            this.btnShowAll.TabIndex = 66;
+            this.btnShowAll.Text = "顯示全員資料";
+            this.btnShowAll.UseVisualStyleBackColor = true;
+            this.btnShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
             // 
             // CustSYSForm
             // 
@@ -314,17 +345,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(206)))), ((int)(((byte)(203)))));
             this.ClientSize = new System.Drawing.Size(1150, 541);
+            this.Controls.Add(this.btnNoright);
             this.Controls.Add(this.txtid);
+            this.Controls.Add(this.btnDrop);
             this.Controls.Add(this.lblid);
+            this.Controls.Add(this.btnCustUpdate);
             this.Controls.Add(this.txtphone);
             this.Controls.Add(this.lblphone);
             this.Controls.Add(this.txtAddress);
             this.Controls.Add(this.txtEmail);
-            this.Controls.Add(this.txtPass);
             this.Controls.Add(this.txtUser);
             this.Controls.Add(this.lbladdress);
             this.Controls.Add(this.lblemail);
-            this.Controls.Add(this.lblpass);
             this.Controls.Add(this.lbluser);
             this.Controls.Add(this.dataCust);
             this.Controls.Add(this.groupBox1);
@@ -333,6 +365,7 @@
             this.Text = "CustSYSForm";
             this.Load += new System.EventHandler(this.CustSYSForm_Load);
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataCust)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -344,19 +377,22 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnNoright;
         private System.Windows.Forms.Button btnDrop;
-        private System.Windows.Forms.Button btnCustInsert;
+        private System.Windows.Forms.Button btnCustUpdate;
         private Guna.UI2.WinForms.Guna2DataGridView dataCust;
         private System.Windows.Forms.TextBox txtphone;
         private System.Windows.Forms.Label lblphone;
         private System.Windows.Forms.TextBox txtAddress;
         private System.Windows.Forms.TextBox txtEmail;
-        private System.Windows.Forms.TextBox txtPass;
         private System.Windows.Forms.TextBox txtUser;
         private System.Windows.Forms.Label lbladdress;
         private System.Windows.Forms.Label lblemail;
-        private System.Windows.Forms.Label lblpass;
         private System.Windows.Forms.Label lbluser;
         private System.Windows.Forms.TextBox txtid;
         private System.Windows.Forms.Label lblid;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox cbxSearch;
+        private System.Windows.Forms.TextBox txtkeyword;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnShowAll;
     }
 }
--- PharmacyShop/CustSYSForm.cs
+++ PharmacyShop/CustSYSForm.cs
@@ -16,6 +16,7 @@ namespace PharmacyShop
     {
         public ProductClass pd = new ProductClass();
         Guna2DataGridView DGV = new Guna2DataGridView();
+        int DGVqty = 0;
         public CustSYSForm()
         {
             InitializeComponent();
@@ -25,16 +26,46 @@ namespace PharmacyShop
         {
             DGV = dataCust;
             pd.ShowData("customer", DGV);
+
+            cbxSearch.Items.Add("name");
+            cbxSearch.Items.Add("phone");
+            cbxSearch.Items.Add("email");
+            cbxSearch.SelectedIndex = 0;
         }
 
-        private void btnCustInsert_Click(object sender, EventArgs e)
+        void ColClear()
         {
-
+            txtid.Clear();
+            txtUser.Clear();
+            txtphone.Clear();
+            txtEmail.Clear();
+            txtAddress.Clear();
         }
 
         private void btnDrop_Click(object sender, EventArgs e)
         {
-
+            ColClear();
+            if (dataCust.SelectedRows.Count > 0)
+            {
+                SqlConnection conn = new SqlConnection(GlobalVar.strDBConnectionString);
+                conn.Open();
+                try
+                {
+                    string strDB = "delete from customer where id = @id";
+                    SqlCommand cmd = new SqlCommand(strDB, conn);
+                    cmd.Parameters.AddWithValue("@id", dataCust.SelectedRows[0].Cells["id"].Value);
+                    if (cmd.ExecuteNonQuery() > 0)
+                    {
+                        MessageBox.Show("資料刪除成功", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        pd.ShowData("customer", DGV);
+                    }
+                    conn.Close();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("資料庫連接失敗: " + ex.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
 
         private void btnNoright_Click(object sender, EventArgs e)
@@ -68,13 +99,28 @@ namespace PharmacyShop
             }
         }
 
-        void ShowCust()
+        void ShowCust(int myid)
         {
             SqlConnection conn = new SqlConnection(GlobalVar.strDBConnectionString);
             conn.Open();
             try
             {
-                string strDB = "";
+                string strDB = "select * from customer where id = @id";
+                SqlCommand cmd = new SqlCommand(strDB, conn);
+                cmd.Parameters.AddWithValue("@id", myid);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    txtid.Text = reader["id"].ToString();
+                    txtUser.Text = reader["name"].ToString();
+                    txtphone.Text = reader["phone"].ToString();
+                    txtEmail.Text = reader["email"].ToString();
+                    txtAddress.Text = reader["address"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("查無此人", "Wrong", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
             }
             catch (Exception ex)
             {
@@ -84,7 +130,75 @@ namespace PharmacyShop
 
         private void dataCust_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            if (dataCust.SelectedRows.Count > 0)
+            {
+                string strID = dataCust.SelectedRows[0].Cells[0].Value.ToString();
+                int selectID = 0;
+                Int32.TryParse(strID, out selectID);
+                ShowCust(selectID);
+            }
+        }
 
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (txtkeyword.Text != "")
+            {
+                string strMsg = cbxSearch.SelectedItem.ToString();
+                SqlConnection con = new SqlConnection(GlobalVar.strDBConnectionString);
+                con.Open();
+                string strDB = $"select id as 會員編號, name as 姓名, phone as 電話, email as 信箱, address as 地址 from customer where {strMsg} like @keyword";
+                SqlCommand cmd = new SqlCommand(strDB, con);
+                cmd.Parameters.AddWithValue("@keyword", $"%{txtkeyword.Text.Trim()}%");
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    dataCust.DataSource = dt;
+                }
+                else
+                {
+                    MessageBox.Show("查無此人", "Wrong", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                reader.Close();
+                con.Close();
+            }
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            pd.ShowData("customer", DGV);
+        }
+
+        private void btnCustUpdate_Click(object sender, EventArgs e)
+        {
+            int intID = 0;
+            Int32.TryParse(txtid.Text, out intID);
+
+            if ((intID > 0) && (txtUser.Text != "") && (txtEmail.Text != ""))
+            {
+                SqlConnection con = new SqlConnection(GlobalVar.strDBConnectionString);
+                con.Open();
+                try
+                {
+                    string strDB = "update customer set name = @name, phone = @phone, email = @email, address = @address where id = @id";
+                    SqlCommand cmd = new SqlCommand(strDB, con);
+                    cmd.Parameters.AddWithValue("@id", txtid.Text);
+                    cmd.Parameters.AddWithValue("@name", txtUser.Text.Trim());
+                    cmd.Parameters.AddWithValue("@phone", txtphone.Text.Trim());
+                    cmd.Parameters.AddWithValue("@email", txtEmail.Text.Trim());
+                    cmd.Parameters.AddWithValue("@address", txtAddress.Text.Trim());
+                    if (cmd.ExecuteNonQuery() > 0)
+                    {
+                        MessageBox.Show("修改成功", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        pd.ShowData("customer", DGV);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("資料庫連接失敗: " + ex.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
         }
     }
 }
